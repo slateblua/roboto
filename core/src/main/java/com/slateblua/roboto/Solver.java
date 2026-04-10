83575e1d391cf877e30d1e958076e5a736157408
slateblua <sega1ness@gmail.com>
2026-04-10 14:06:55 +0400

    Update Solver contract

--- core/src/main/java/com/slateblua/roboto/Solver.java
+++ core/src/main/java/com/slateblua/roboto/Solver.java
@@ -1,10 +1,12 @@
 package com.slateblua.roboto;
 
 import com.badlogic.gdx.utils.Array;
-import com.slateblua.roboto.data.Angle;
+import com.slateblua.roboto.data.Segment;
 import com.slateblua.roboto.data.Tuple;
 
 public interface Solver {
     // Gets a target position and returns a list of angles to reach it.
-    Array<Angle> solve(final Tuple target);
+    void solve(final Tuple target, final Array<Segment> segment);
+
+    void setBase(float x, float y);
 }
