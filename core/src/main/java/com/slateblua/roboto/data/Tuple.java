0c7400f658b95e7ec2cb9a3f5e146468374befe6
slateblua <sega1ness@gmail.com>
2026-04-10 14:06:41 +0400

    Add helpers to Tuple for dst and angleDeg calculations

--- core/src/main/java/com/slateblua/roboto/data/Tuple.java
+++ core/src/main/java/com/slateblua/roboto/data/Tuple.java
@@ -1,9 +1,21 @@
 package com.slateblua.roboto.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Tuple {
     private final float x;
     private final float y;
+
+    public float dst(Tuple other) {
+        float dx = x - other.x;
+        float dy = y - other.y;
+        return (float) Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public float angleDeg(Tuple other) {
+        return (float) Math.toDegrees(Math.atan2(other.y - y, other.x - x));
+    }
 }
