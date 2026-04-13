74fe9d5adb13d57be2525798e3edfe7b23e3f073
slateblua <sega1ness@gmail.com>
2026-04-13 15:26:28 +0400

    Remove unnecessary configuration setup

--- lwjgl3/src/main/java/com/slateblua/roboto/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/slateblua/roboto/lwjgl3/Lwjgl3Launcher.java
@@ -11,8 +11,8 @@ public class Lwjgl3Launcher {
         createApplication();
     }
 
-    private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new RobotoEntry(), getDefaultConfiguration());
+    private static void createApplication() {
+        new Lwjgl3Application(new RobotoEntry(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
@@ -32,17 +32,6 @@ public class Lwjgl3Launcher {
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
-
-        //// This could improve compatibility with Windows machines with buggy OpenGL drivers, Macs
-        //// with Apple Silicon that have to emulate compatibility with OpenGL anyway, and more.
-        //// This uses the dependency `com.badlogicgames.gdx:gdx-lwjgl3-angle` to function.
-        //// You would need to add this line to lwjgl3/build.gradle , below the dependency on `gdx-backend-lwjgl3`:
-        ////     implementation "com.badlogicgames.gdx:gdx-lwjgl3-angle:$gdxVersion"
-        //// You can choose to add the following line and the mentioned dependency if you want; they
-        //// are not intended for games that use GL30 (which is compatibility with OpenGL ES 3.0).
-        //// Know that it might not work well in some cases.
-//        configuration.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.ANGLE_GLES20, 0, 0);
-
         return configuration;
     }
 }
